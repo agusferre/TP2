95f5b282a32e3781c58cbd9b081ebb242a93b08f
pau-gonzalez <paula.cgs@hotmail.com>
2017-05-31 17:11:25 -0300

    insert_org_assign

--- map.h
+++ map.h
@@ -1315,13 +1315,31 @@ x:p:left D y
      * \T{Meaning} tenga constructor sin parámetros.  La desventaja es que la notación no es tan bonita.
      */
     iterator insert_or_assign(const_iterator hint, const value_type& value) {
-
+    	iterator it;
+    	if (hint.n->value()->first == value.first){ 
+    		it = hint;
+    		it.n->value()->second = value->second;
+    	} else if (hint.n->value()->first < value->first() || (hint--.n == nullptr 
+    		|| hint.n->value()->first >= value->first)){
+    		it = lower_bound(hint->_value);
+		}
+		it = insert(hint, value);
+		return it;
     }
 
     /** \overload */
     iterator insert_or_assign(const value_type& value) {
-    	//completar
-    }
+		iterator it;
+    	if (hint.n->value()->first == value.first){ 
+    		it = hint;
+    		it.n->value()->second = value->second;
+    	} else if (hint.n->value()->first < value->first() || (hint--.n == nullptr 
+    		|| hint.n->value()->first >= value->first)){
+    		it = lower_bound(hint->_value);
+		}
+		it = insert(hint, value);
+		return it;    
+	}
 
     /**
      * @brief Elimina el valor apuntado por \P{pos}
