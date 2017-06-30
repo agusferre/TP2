6a9d31d24219c02bcaceabe86c2999d2425d1cfa
pau-gonzalez <paula.cgs@hotmail.com>
2017-06-30 00:20:18 -0300

    pauli

--- html/namespaceaed2.js
+++ html/namespaceaed2.js
@@ -1,8 +1,4 @@
 var namespaceaed2 =
 [
-<<<<<<< HEAD
     [ "map", "classaed2_1_1map.html", null ]
-=======
-    [ "map", "classaed2_1_1map.html", "classaed2_1_1map" ]
->>>>>>> 0cf21500dcc60bdd14dbf4470236b5159feeb7ce
 ];
--- html/namespaces.js
+++ html/namespaces.js
@@ -1,8 +1,4 @@
 var namespaces =
 [
-<<<<<<< HEAD
     [ "aed2", "namespaceaed2.html", "namespaceaed2" ]
-=======
-    [ "aed2", "namespaceaed2.html", null ]
->>>>>>> 0cf21500dcc60bdd14dbf4470236b5159feeb7ce
 ];
